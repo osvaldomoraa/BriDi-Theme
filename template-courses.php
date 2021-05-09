28c361ddb381632c03b1580280de2168ea11ce6a
Osvaldo Mora <osvaldo.moraa@yahoo.com.mx>
2021-05-09 17:32:37 -0500

    page template de cursos

---
+++ template-courses.php
@@ -0,0 +1,48 @@
+<?php
+
+//Template Name: Pagina de talleres
+
+user_logged_verification();
+
+function page_styles(){   
+    wp_enqueue_style( 'page' );
+}
+add_action('wp_enqueue_scripts','page_styles');
+
+get_header();?>
+
+<main>
+    <div class="nav-divider d-flex justify-content-center">
+        <div class="triangle-down "></div>
+    </div>
+    <section id="page-section">
+        <div class='page-background-wrapper'></div>
+        <div class="container">
+            <div class="row page-heading-row align-items-center justify-content-center">
+                <div class="col-12">
+                    <div class="page-title-box">
+                        <h1 class='standard-title page-title'>
+                            <?php the_title();?>
+                        </h1>                        
+                    </div>                        
+                </div>
+            </div>
+            <div class="row page-content-row align-items-center justify-content-center">
+                <div class="col-12">
+                    <div class="page-content-box">
+                        <?php if(have_posts()){
+                            while(have_posts()){ the_post();
+                                the_content();
+                            }
+                        }?>                        
+                    </div>                        
+                </div>
+            </div>
+        </div>            
+    </section>
+    <div class="divider-wrapper">
+        <div class="divider-h-p1"></div>
+    </div>
+</main>
+
+<?php get_footer();?>
